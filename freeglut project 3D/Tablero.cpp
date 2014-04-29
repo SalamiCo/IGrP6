f4520547ae3f5a24b5d588005354a5a24d42589f
Pedro Morgado <p.morgado107@gmail.com>
2014-04-29 12:27:15 +0200

    Tablero class

--- freeglut project 3D/Tablero.cpp
+++ freeglut project 3D/Tablero.cpp
@@ -5,6 +5,14 @@ Tablero::Tablero(void)
 {
 }
 
+Tablero::Tablero(float a, float l, float g, int pa, int pl, int pg){
+	this->anchoT = a;
+	this->largoT = l;
+	this->gruesoT = g;
+	this->partesAncho = pa;
+	this->partesLargo = pl;
+	this->partesGrueso = pg;
+}
 
 Tablero::~Tablero(void)
 {
--- freeglut project 3D/Tablero.h
+++ freeglut project 3D/Tablero.h
@@ -1,10 +1,31 @@
 #pragma once
+
+#include <Windows.h>
+#include <gl/GL.h>
+#include <gl/GLU.h>
+
+#include <GL/freeglut.h>
+#include <GL/glut.h>
+
 #include "malla.h"
 class Tablero :
 	public Malla
 {
+private:
+	float anchoT, largoT, gruesoT;
+	int partesAncho, partesLargo, partesGrueso;
+
 public:
 	Tablero(void);
+	Tablero(float a, float l, float g, int pa, int pl, int pg);
 	~Tablero(void);
+
+	float getAncho(){return this->anchoT;}
+	float getLargo(){return this->largoT;}
+	float getGrueso(){return this->gruesoT;}
+
+	int getPartesAncho(){return this->partesAncho;}
+	int getPartesLargo(){return this->partesLargo;}
+	int getPartesGrueso(){return this->partesGrueso;}
 };
 
