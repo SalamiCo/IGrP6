5fc6f311e404503435fb96aa5ef7008076549a82
Pedro Morgado <p.morgado107@gmail.com>
2014-04-30 02:08:12 +0200

    TColor class

--- freeglut project 3D/TColor.h
+++ freeglut project 3D/TColor.h
@@ -1,8 +1,20 @@
 #pragma once
 class TColor
 {
+private:
+	float red, green, blue;
+
 public:
 	TColor(void);
+	TColor(float r, float g, float b);
 	~TColor(void);
+
+	float getRed(){ return this->red; }
+	float getGreen(){ return this->green; }
+	float getBlue(){ return this->blue; }
+
+	void setRed(float r){ this->red = r; }
+	void setGreen(float g){ this->green = g; }
+	void setBlue(float b){ this->blue = b; }
 };
 
